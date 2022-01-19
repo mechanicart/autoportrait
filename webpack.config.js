659313871af78771d03c75a3f80254d3d2b1b661
Tikhomirov Artem Igorevich <tikhomirov.artem.i@gmail.com>
2022-01-19 12:02:06 +0300

    run a hello world

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.ts$/,
+        test: /\.(ts|tsx)$/,
         exclude: /node_modules/,
         use: [
           { loader: 'babel-loader' }
