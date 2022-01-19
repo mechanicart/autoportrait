89a10594ba8e8d1fb39b7d90c68cac0eb72657ef
Tikhomirov Artem Igorevich <tikhomirov.artem.i@gmail.com>
2022-01-19 11:46:02 +0300

    add a eslint plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,22 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin')
 
 module.exports = {
   context: __dirname,
   entry: path.join(__dirname, 'src', 'index.tsx'),
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'build.js'
+    filename: 'bundle.js'
   },
   mode: 'development',
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
   plugins: [
+    new ESLintPlugin({
+      extensions: ['.tsx', '.ts', '.js'],
+    }),
     new HtmlWebpackPlugin({
       template: path.join(__dirname, "public", "index.html"),
     }),
