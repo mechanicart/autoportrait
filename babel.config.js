d4fb388e2574763e2254fa8c14a44bc5d2ea20c5
Tikhomirov Artem Igorevich <tikhomirov.artem.i@gmail.com>
2022-01-19 11:18:15 +0300

    add a core-js

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,13 @@
 module.exports = {
-  presets: [
-    "@babel/preset-env"
+  "presets": [
+    "@babel/preset-typescript",
+    [
+      "@babel/preset-env",
+      {
+        "corejs": 3,
+        "useBuiltIns": "entry",
+        "targets": "> 0.25%, not dead"
+      }
+    ]
   ]
 }
