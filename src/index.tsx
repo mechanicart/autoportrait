0bde87a5e352f4e29471d8c198f2c93bfae1e6e4
Tikhomirov Artem Igorevich <tikhomirov.artem.i@gmail.com>
2022-01-20 13:33:10 +0300

    add an eslint webpack plugin

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
+import { App } from './App';
 
 ReactDOM.render(
-  <h1>Hello World</h1>,
-  document.querySelector('#root')
+  <App />,
+  document.querySelector('#root'),
 );
