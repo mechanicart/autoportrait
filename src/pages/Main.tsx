842207542be62d32a6c233d1bd5b8fe47fcd71fe
Tikhomirov Artem Igorevich <tikhomirov.artem.i@gmail.com>
2022-02-02 14:46:56 +0300

    add a hradient to the main page

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,13 +1,32 @@
 import { FC } from 'react';
-import styled from 'styled-components/macro';
+import styled, { keyframes } from 'styled-components/macro';
 import { Page } from '../components/Page';
 
-const Header = styled.h1`
+const gradient = keyframes`
+{
+0% {
+  background-position: 0 50%;
+}
+50% {
+  background-position: 100% 50%;
+}
+100% {
+  background-position: 0 50%;
+}}
+`;
+
+const AnimatedHeader = styled.h1`
   font-size: 2em;
+  animation: ${gradient} 5s ease-in-out infinite;
+  background: linear-gradient(45deg, rgb(156, 14, 156), midnightblue);
+  background-size: 300%;
+  background-clip: text;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
 `;
 
 export const Main: FC = () => (
   <Page>
-    <Header>👋 , I&apos;m Artem!</Header>
+    <AnimatedHeader>Hi, I&apos;m Artem!</AnimatedHeader>
   </Page>
 );
