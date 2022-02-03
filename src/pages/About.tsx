1da12bac258e9bb9d8670e1314acdae3b1b5e9c5
Tikhomirov Artem Igorevich <tikhomirov.artem.i@gmail.com>
2022-02-03 10:47:41 +0300

    add a draft of the content of an about page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -9,5 +9,11 @@ const Header = styled.h1`
 export const About: FC = () => (
   <Page>
     <Header>About page</Header>
+    <div>
+      &ldquo;Art&apos;em&rdquo; is a short form of &ldquo;art them&rdquo;. If
+      you say &ldquo;art them&rdquo; very fast, without carefully pronouncing
+      each word, it can sound like Artem. Nice to meet you! Artem is my name.
+    </div>
+    <div>I am a Frontend developer with a wide range of experience.</div>
   </Page>
 );
