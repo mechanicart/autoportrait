9198a81d3a03ab66bc824008147251148fb068cc
Tikhomirov Artem Igorevich <tikhomirov.artem.i@gmail.com>
2022-02-14 11:41:17 +0300

    fix configurations

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,6 +3,8 @@ const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
@@ -11,7 +13,26 @@ module.exports = merge(common, {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
   },
-  plugins: [new CleanWebpackPlugin()],
+  plugins: [
+    new CleanWebpackPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'public'),
+          to: path.resolve(__dirname, 'dist'),
+          globOptions: {
+            ignore: ['*.DS_Store', '**/index.html'],
+          },
+        },
+      ],
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: path.join(__dirname, 'public', 'index.html'),
+      chunks: ['main'],
+      favicon: './public/favicon.png',
+    }),
+  ],
   optimization: {
     minimize: true,
   },
